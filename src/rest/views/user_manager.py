6e639711e37a7c44646e6e9fd7e7b7294d65126a
kenigteh <artemon1002@gmail.com>
2019-09-22 01:26:16 +0200

    Back to 2007

--- src/rest/views/user_manager.py
+++ src/rest/views/user_manager.py
@@ -22,12 +22,10 @@ class UserManager(APIView):
         login = request.headers.get('login')
         password = request.headers.get('password')
         try:
-            user = User.objects.get(login=login)
+            user = User.objects.get(login=login, password=password)
             data = {
                 'login': user.login,
-                'password': user.password,
             }
-            serializer = UserSerializer(user)
             return Response(data, status=200)
         except:
             return Response({'status': 'User not found'}, status=400)
